d8201d56ba0bbefb73a05e277936c014d4c07da3
Gorea Tudor <tudortudor2002@gmail.com>
2021-10-13 11:32:14 +0300

    swapped back buttons for a menu option added menus to Log, Sign In and Sign Up frames menus contain back (except Log), and exit

--- src/GUI/LogWindow/LogFrame.java
+++ src/GUI/LogWindow/LogFrame.java
@@ -2,9 +2,15 @@ package GUI.LogWindow;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class LogFrame extends JFrame {
+public class LogFrame extends JFrame implements ActionListener {
     public static LogFrame logFrame;
+
+    JMenuBar menuBar;
+    JMenu fileMenu;
+    JMenuItem exitItem;
 
     SignUpButtonLink signUpButton;
     SignInButtonLink signInButton;
@@ -15,12 +21,27 @@ public class LogFrame extends JFrame {
         this.setLayout(new FlowLayout());
         this.setTitle("Accounts");
 
+        menuBar = new JMenuBar();
+        fileMenu = new JMenu("File");
+        exitItem = new JMenuItem("Exit");
+        exitItem.addActionListener(this);
+
+        fileMenu.add(exitItem);
+        menuBar.add(fileMenu);
+
+
         signUpButton = new SignUpButtonLink();
         signInButton = new SignInButtonLink();
 
+        this.setJMenuBar(menuBar);
         this.add(signUpButton);
         this.add(signInButton);
 
         this.setVisible(true);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        System.exit(0);
     }
 }
